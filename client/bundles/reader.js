fd7c1d422f24229d119f288bd5e7f2af8dd31c97
Mauricio Rivera <maurrivera@gmail.com>
2013-06-17 20:52:13 -0700

    reader.jade creation

--- client/bundles/reader.js
+++ client/bundles/reader.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+  var last=$(document).scrollTop()
+  var offset=last
   setInterval(function(){
     console.log("interval")
+    last=offset
+    offset=$(document).scrollTop()
+    var delta=(offset-last)
+    console.log(delta)  
+    if(delta<-200){
+      $(".topnav").addClass("fixed")
+    }
+    if(delta>200){
+      $(".topnav").removeClass("fixed")
+    }
   },100)
 })
+
